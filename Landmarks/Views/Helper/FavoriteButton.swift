980eb3c14251125450f0219c9dc758a8f161f88d
kmh339 <kmh339@gmail.com>
2024-04-07 15:18:10 +0900

    add FavoriteButton

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by red on 4/5/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
